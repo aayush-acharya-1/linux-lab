85f6fde9e0ddf36e77ce2129ef41c98c88186a7d
aayush-acharya-1 <aayush.acharya@bison.howard.edu>
2023-04-22 20:19:54 -0400

    provide formatting to the encrypted text

--- mycipher.py
+++ mycipher.py
@@ -1,5 +1,23 @@
 import sys
 
+def format_output(output):
+    row = ""
+    lines = ""
+    row_count = 0
+    for letter in output:
+        row += letter 
+        if len(row) == 5 and row_count < 9:
+            row += " "
+            lines += row
+            row_count += 1
+            row = ""
+        elif len(row) == 5 and row_count == 9:
+            lines += row +"\n"
+            row_count = 0
+            row = ""
+
+    return(lines + row + "\n")
+
 def encryption(message, shift):
     message = message.upper()
     characters = ""
@@ -24,10 +42,11 @@ def encryption(message, shift):
     
       encoded_text.append(chr(new_ascii))
 
-    return ''.join(encoded_text)
-
+    value = ''.join(encoded_text)
+    return format_output(value)
+   
 args = sys.argv
 
 for line in sys.stdin:
     result = encryption(line.strip(), int(args[1]))
-    print(result)
+    sys.stdout.write(result)
